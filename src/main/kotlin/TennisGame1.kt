55caff1c56fbc97a179ca8c6f1c2c2126da07d8c
Eisenstaedter, Thomas (SE-A/34) <thomas.eisenstaedter@volkswagen.de>
2024-05-23 10:01:52 +0200

    [WIP] refactoring

--- src/main/kotlin/TennisGame1.kt
+++ src/main/kotlin/TennisGame1.kt
@@ -3,12 +3,7 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
     private var m_score1: Int = 0
     private var m_score2: Int = 0
 
-    override fun wonPoint(playerName: String) {
-        if (playerName === "player1")
-            m_score1 += 1
-        else
-            m_score2 += 1
-    }
+    override fun wonPoint(playerName: String) = if (playerName === "player1") m_score1 += 1 else m_score2 += 1
 
     override fun getScore(): String {
         var score = ""
